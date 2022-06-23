1ceb586580d5b09ad57ee3967b98673fbea83aab
Ivaylo22 <ivi_petkow@abv.bg>
2022-06-23 18:32:09 +0300

    Added Exam Prep 01

---
+++ Exam Prep 1/Exam Prep 1/01. Bakery Shop/Program.cs
@@ -0,0 +1,96 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _01._Bakery_Shop
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            double[] waterInput = Console.ReadLine().Split(' ').Select(double.Parse).ToArray();
+            Queue<double> waters = new Queue<double>(waterInput);
+
+            double[] flourInput = Console.ReadLine().Split(' ').Select(double.Parse).ToArray();
+            Stack<double> flours = new Stack<double>(flourInput);
+            var products = new Dictionary<string, int>();
+
+            while (waters.Count != 0 && flours.Count != 0) {
+                bool isValid = true;
+                double flourLeft = 0;
+                double currentWater = waters.Peek();
+                double currentFlour = flours.Peek();
+
+                double sum = currentFlour + currentWater;
+
+                double percentWater = currentWater * 100 / sum;
+                double percentFlour = currentFlour * 100 / sum;
+
+                if (percentWater == 40 && percentFlour == 60) {
+                    if (!products.Keys.Contains("Muffin"))
+                    {
+                        products.Add("Muffin", 0);
+                    }
+                    products["Muffin"]++;
+                }
+
+                else if (percentWater == 30 && percentFlour == 70)
+                {
+                    if (!products.Keys.Contains("Baguette"))
+                    {
+                        products.Add("Baguette", 0);
+                    }
+                    products["Baguette"]++;
+                }
+
+                else if (percentWater == 20 && percentFlour == 80)
+                {
+                    if (!products.Keys.Contains("Bagel"))
+                    {
+                        products.Add("Bagel", 0);
+                    }
+                    products["Bagel"]++;
+                }
+
+                else if (percentWater == 50 && percentFlour == 50) 
+                {             
+                    if (!products.Keys.Contains("Croissant"))
+                    {
+                        products.Add("Croissant", 0);
+                    }
+                    products["Croissant"]++;                   
+                }
+
+                else 
+                {
+                    if (!products.Keys.Contains("Croissant"))
+                    {
+                        products.Add("Croissant", 0);
+                    }
+                    flourLeft = currentFlour - currentWater;
+                    products["Croissant"]++;
+                    isValid = false;
+                    flours.Pop();
+                    flours.Push(flourLeft);
+                }
+
+                waters.Dequeue();
+                flours.Pop();
+                if (!isValid)
+                    flours.Push(flourLeft);
+            }
+
+            foreach (string product in products.Keys.OrderByDescending(product => products[product]).ThenBy(product => product)) {
+                Console.WriteLine($"{product}: {products[product]}");
+            }
+            if (waters.Count == 0) 
+                Console.WriteLine("Water left: None");
+            else
+                Console.WriteLine($"Water left: {string.Join(", ", waters)}");
+            if (flours.Count == 0)
+                Console.WriteLine("Flour left: None");
+            else
+                Console.WriteLine($"Flour left: {string.Join(", ", flours)}");
+        }
+    }
+}
---
+++ Exam Prep 1/Exam Prep 1/02. Beaver at Work/Program.cs
@@ -0,0 +1,214 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _02._Beaver_at_Work
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            int beaverRow = 0;
+            int beaverCol = 0;
+            int totalBranches = 0;
+            int currentBranches = 0;
+            bool foundAll = false;
+
+            char[,] matrix = new char[n,n];
+
+            Stack<char> branches = new Stack<char>();
+
+            for (int row = 0; row < n; row++)
+            {
+                char[] chars = Console.ReadLine().Replace(" ", "").ToCharArray();
+
+                for (int col = 0; col < n; col++) { 
+                    matrix[row,col] = chars[col];
+                    if (matrix[row, col].Equals('B'))
+                    {
+                        beaverRow = row;
+                        beaverCol = col;
+                    }
+                    else if (char.IsLower(matrix[row, col])) {
+                        totalBranches++;
+                    }
+                }
+            }
+
+            string input = Console.ReadLine();
+            while (input != "end") 
+            {
+                if (input.Equals("up")) {
+                    matrix[beaverRow, beaverCol] = '-';
+                    beaverRow--;
+                    if (beaverRow < 0)
+                    {
+                        if(branches.Count > 0)
+                            branches.Pop();
+                        beaverRow++;
+                    }
+                    else if (char.IsLower(matrix[beaverRow, beaverCol]))
+                    {
+                        branches.Push(matrix[beaverRow, beaverCol]);
+                        currentBranches++;
+                        matrix[beaverRow, beaverCol] = '-';
+                    }
+                    else if (matrix[beaverRow, beaverCol] == 'F') 
+                    {
+                        matrix[beaverRow, beaverCol] = '-';
+                        if (beaverRow == 0)
+                        {
+                            while (beaverRow < matrix.GetLength(0) - 1)
+                                beaverRow++;
+                        }
+                        else
+                        {
+                            while (beaverRow >= 0)
+                                beaverRow--;
+                        }
+
+                        if (char.IsLower(matrix[beaverRow, beaverCol]))
+                        {
+                            branches.Push(matrix[beaverRow, beaverCol]);
+                            currentBranches++;
+                        }
+                        matrix[beaverRow, beaverCol] = '-';
+                    }
+                }
+
+                if (input.Equals("down"))
+                {
+                    matrix[beaverRow, beaverCol] = '-';
+
+                    beaverRow++;
+                    if (beaverRow > matrix.GetLength(0) - 1)
+                    {
+                        if (branches.Count > 0)
+                            branches.Pop();
+                        beaverRow--;
+                    }
+                    else if (char.IsLower(matrix[beaverRow, beaverCol]))
+                    {
+                        branches.Push(matrix[beaverRow, beaverCol]);
+                        currentBranches++;
+                        matrix[beaverRow, beaverCol] = '-';
+                    }
+                    else if (matrix[beaverRow, beaverCol].Equals('F'))
+                    {
+                        matrix[beaverRow, beaverCol] = '-';
+                        if (beaverRow == matrix.GetLength(0) - 1)
+                        {
+                            while (beaverRow >= 0)
+                                beaverRow--;
+                        }
+                        else
+                        {
+                            while (beaverRow < matrix.GetLength(0))
+                                beaverRow++;
+                        }
+                    }
+                }
+
+                if (input.Equals("right"))
+                {
+                    matrix[beaverRow, beaverCol] = '-';
+
+                    beaverCol++;
+                    if (beaverCol > matrix.GetLength(1) - 1)
+                    {
+                        if (branches.Count > 0)
+                            branches.Pop();
+                        beaverCol--;
+                    }
+                    else if (char.IsLower(matrix[beaverRow, beaverCol]))
+                    {
+                        branches.Push(matrix[beaverRow, beaverCol]);
+                        currentBranches++;
+                        matrix[beaverRow, beaverCol] = '-';
+                    }
+                    else if (matrix[beaverRow, beaverCol].Equals('F'))
+                    {
+                        matrix[beaverRow, beaverCol] = '-';
+                        if (beaverCol == matrix.GetLength(1) - 1)
+                        {
+                            while (beaverCol >= 0)
+                                beaverCol--;
+                        }
+                        else
+                        {
+                            while (beaverCol < matrix.GetLength(1))
+                                beaverCol++;
+                        }
+                    }
+                }
+
+                if (input.Equals("left"))
+                {
+                    matrix[beaverRow, beaverCol] = '-';
+
+                    beaverCol--;
+                    if (beaverCol < 0)
+                    {
+                        if (branches.Count > 0)
+                            branches.Pop();
+                        beaverCol++;
+                    }
+                    else if (char.IsLower(matrix[beaverRow, beaverCol]))
+                    {
+                        branches.Push(matrix[beaverRow, beaverCol]);
+                        currentBranches++;
+                        matrix[beaverRow, beaverCol] = '-';
+                    }
+                    else if (matrix[beaverRow, beaverCol].Equals('F'))
+                    {
+                        matrix[beaverRow, beaverCol] = '-';
+                        if (beaverCol == 0)
+                        {
+                            while (beaverCol < matrix.GetLength(1))
+                                beaverCol++;
+                        }
+                        else
+                        {
+                            while (beaverCol >= 0)
+                                beaverCol--;
+                        }
+                    }
+                }
+                matrix[beaverRow, beaverCol] = 'B';
+                if (currentBranches == totalBranches) { 
+                    foundAll = true;
+                    break;
+                }
+
+                input = Console.ReadLine();
+            }
+            int branchesLeft = 0;
+            for (int row = 0; row < matrix.GetLength(0); row++)
+            {
+                for (int col = 0; col < matrix.GetLength(1); col++)
+                {
+                    if (char.IsLower(matrix[row, col]))
+                        branchesLeft++;
+                }
+            }
+            List<char> branchesList = new List<char>(branches);
+            branchesList.Reverse();
+
+            if (foundAll)
+                Console.WriteLine($"The Beaver successfully collect {branches.Count} wood branches: {string.Join(", ", branchesList)}.");
+            else
+                Console.WriteLine($"The Beaver failed to collect every wood branch. There are {branchesLeft} branches left.");
+
+            for (int row = 0; row < matrix.GetLength(0); row++)
+            {
+                for (int col = 0; col < matrix.GetLength(1); col++)
+                {
+                    Console.Write(matrix[row, col] + " ");
+                }
+                Console.WriteLine();
+            }
+        }
+    }
+}
